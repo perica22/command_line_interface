d06cfa553d1083c15bced32ecb9776a48f742905
Perica Prokic <pprokic22@gmail.com>
2019-06-11 16:42:07 +0200

    setup file with additional packages

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip install click
-wait
-pip install json
-wait
-pip install requests
--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     name = 'cgccli',
     version = '0.1.0',
     packages = ['cgccli'],
+    install_requires = ['click',
+			'requests'],
     entry_points = {
         'console_scripts': [
             'cgccli = cgccli.__main__:main'
