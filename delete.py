f9d328f54397164dbe55dabf8ca7e58f20d0c38e
perica22 <pprokic22@gmail.com>
2019-06-12 21:32:03 +0200

    ..

--- delete.py
+++ delete.py
@@ -17,11 +17,6 @@ from cgccli.cgccli_controller import CgccliController
 @click.option('--dest', required=False, default=None, show_default=True, help='Destination to download files.')
 def main(token, argument, project, file, data, dest):
 
-    import ipdb
-    ipdb.set_trace()
-    shipping_address_data = {'billing_address[province_state]': u'Michigan', 'shipping_address[last_name]': u'Candy', 'shipping_address[first_name]': u'Lolipop', 'billing_address[phone]': u'', 'shipping_address[province_state]': u'Michigan', 'billing_address[postalcode]': u'48212', 'billing_address[first_name]': u'Lolipop', 'billing_address[address_1]': u'2017 Nikola Tesla Dr', 'shipping_address[city]': u'Detroit', 'billing_is_shipping': u'0', 'shipping_address[phone]': u'', 'shipping_address[address_1]': u'2015 Nikola Tesla Dr', 'billing_address[country]': u'United States', 'shipping_address[country]': u'United States', 'billing_address[email]': u'example_mail@gmail.com', 'shipping_address[postalcode]': u'48212', 'billing_address[company]': u'', 'billing_address[city]': u'Detroit', 'shipping_address[company]': u'', 'billing_address[last_name]': u'Candy', 'shipping_address[address_2]': u'', 'billing_address[address_2]': u''}
-    for k,v in shipping_address_data.items():
-        print(k)
     cgccli = CgccliController(
         token, file, data, dest)
 
